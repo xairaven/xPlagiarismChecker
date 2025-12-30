9ffbb28f5bb23c9a5d46fec28c6e721135ddf8d6
xairaven <alexkovalevkpi@gmail.com>
2025-12-30 02:19:03 +0200

    (Engine) Feat: Re-export library types and modules for improved accessibility

--- engine/src/lib.rs
+++ engine/src/lib.rs
@@ -1,3 +1,14 @@
+// Re-exports
+/// Library error type. Critical
+pub use errors::LibError;
+/// Library error type for bad files, can be shown for users
+pub use io::FileError;
+/// Loading submissions from files
+pub use io::FileLoader;
+/// Database model
+pub use models::database::Database;
+
+// Modules
 pub mod errors;
 pub mod io;
 pub mod models;
